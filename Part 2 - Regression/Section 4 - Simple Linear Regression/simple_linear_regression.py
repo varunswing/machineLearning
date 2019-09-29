300dd6f2ae214a8f0171a3e557b79d08197ad3c1
varunswing <varuns939@gmail.com>
2019-09-30 01:00:39 +0530

    Update simple_linear_regression.py

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -1,42 +1,47 @@
-import math
-import os
-import random
-import re
-import sys
+# Simple Linear Regression
 
-class SinglyLinkedListNode:
-    def __init__(self, node_data):
-        self.data = node_data
-        self.next = None
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+ 
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
 
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-    def insert_node(self, node_data):
-        node = SinglyLinkedListNode(node_data)
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
 
-        if not self.head:
-            self.head = node
-        else:
-            self.tail.next = node
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
 
-        self.tail = node
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
-# Complete the printLinkedList function below.
-
-#
-# For your reference:
-#
-# SinglyLinkedListNode:
-#     int data
-#     SinglyLinkedListNode next
-#
-#
-def printLinkedList(head):
-    node = head
-    while node.tail != None:
-        print(node.data)
-        node.next = node
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
